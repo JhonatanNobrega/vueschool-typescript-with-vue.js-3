4b217ec91b3aa278fd594e096931582f1ff34e31
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2024-04-13 08:46:56 -0300

    Aula 1 a 16

--- src/components/EntryCard.vue
+++ src/components/EntryCard.vue
@@ -1,18 +1,31 @@
 <script lang="ts" setup>
 import DateDisplay from "./DateDisplay.vue";
 import UseEmojis from "@/composables/UseEmojis";
+import type Entry from './../types/Entry';
+import { inject } from "vue";
+import { userInjectionKey } from "@/injectionKeys";
+
+// data
+const user = inject(userInjectionKey);
+
+// props
+defineProps<{
+  entry: Entry
+}>();
+
+// composables
 const { findEmoji } = UseEmojis();
+
 </script>
 <template>
   <div class="entry-card">
     <div class="entry-card-body">
-      <component width="75" :is="findEmoji('happy')"></component>
-      <div class="entry-text">Today I enjoyed walking the dog in the park.</div>
+      <component width="75" :is="findEmoji(entry.emoji)"></component>
+      <div class="entry-text">{{ entry.body }}</div>
     </div>
     <div class="entry-footer">
-      <DateDisplay :date="new Date()" class="mr-2" />
-      |
-      <span class="ml-2">danielkelly_io</span>
+      <DateDisplay :date="entry.createdAt" class="mr-2" /> |
+      <span class="ml-2">{{user?.username || 'anonymous'}}</span>
     </div>
   </div>
 </template>
--- src/composables/UseEmojis.ts
+++ src/composables/UseEmojis.ts
@@ -1,5 +1,6 @@
-import type { Component } from "vue";
 import { reactive } from "vue";
+import type Emoji from "@/types/Emoji";
+import type { Component } from "vue";
 
 const files: { [key: string]: unknown } = import.meta.glob(
   "../assets/icons/emojis/*.svg",
@@ -18,7 +19,7 @@ export default function UseEmojis() {
   /**
    * Get a single emoji component by name
    */
-  const findEmoji = (name: string) =>
+  const findEmoji = (name: Emoji | null) =>
     emojis.find((emoji) => emoji.name === name)?.component;
 
   return { emojis, findEmoji };
